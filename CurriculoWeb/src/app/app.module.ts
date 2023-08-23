f19e3948b44688a8ecd232fcb24fbaca34f7c793
Felipe da Silva Rodrigues <felipedasr3232@gmail.com>
2023-08-22 23:42:06 -0300

    Criação da branch feature/card e do componente Card

--- CurriculoWeb/src/app/app.module.ts
+++ CurriculoWeb/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SideBarComponent
+    SideBarComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
